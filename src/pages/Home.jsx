67b3b37b61b8b758a5571ab94b450df77a9afef5
Ankit-Bidhan <bidhanankit06@gmail.com>
2026-08-16 20:14:01 +0530

    Temporarily disable nearby places section to restore stable homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,14 +3,12 @@ import ExploreDestinations from "../components/ExploreDestinations";
 import HiddenGems from "../components/HiddenGems";
 import CrowdInsights from "../components/CrowdInsights";
 import AIGuide from "../components/AIGuide";
-import NearbyPlaces from "../components/NearbyPlaces";
 
 function Home() {
     return (
         <>
             <Hero />
             <ExploreDestinations />
-            <NearbyPlaces />
             <HiddenGems />
             <CrowdInsights />
             <AIGuide />
